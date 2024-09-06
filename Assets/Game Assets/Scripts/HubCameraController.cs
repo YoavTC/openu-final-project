877fd570266078c5e83c30d57afd092ea72be140
Yoav TC <yoavtc2004@gmail.com>
2024-09-06 18:02:52 +0300

    🖼️• Added basic hub pages

--- Assets/Game Assets/Scripts/HubCameraController.cs
+++ Assets/Game Assets/Scripts/HubCameraController.cs
@@ -1,23 +1,53 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using NaughtyAttributes;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class HubCameraController : MonoBehaviour, IDragHandler, IEndDragHandler
+public class HubCameraController : MonoBehaviour
 {
-    private Vector3 panelLocation;
+    [Header("Settings")]
+    [SerializeField] private float swipeTransitionDuration;
+    [SerializeField] private Ease swipeTransitionEaseType;
+    [SerializeField] private int initialPanel;
     
-    public void OnDrag(PointerEventData eventData)
+    private List<RectTransform> hubPanels = new List<RectTransform>();
+    private RectTransform panelsContainer;
+    private float panelWidth;
+    
+    private void Start()
     {
-        float difference = eventData.pressPosition.x - eventData.position.x;
-        transform.position = panelLocation - new Vector3(difference, 0, 0);
+        panelsContainer = transform.GetChild(0).GetComponent<RectTransform>();
+        panelWidth = panelsContainer.GetChild(0).GetComponent<RectTransform>().rect.width;
+        HubPanelsSetup();
     }
 
-    public void OnEndDrag(PointerEventData eventData)
+    private void HubPanelsSetup()
     {
-        panelLocation = transform.position;
+        //Add hub panels to list 
+        hubPanels.Clear();
+        for (int i = 0; i < panelsContainer.childCount; i++)
+        {
+            hubPanels.Add(panelsContainer.GetChild(i).GetComponent<RectTransform>());
+            
+            if (i == 0)
+            {
+                hubPanels[i].anchoredPosition = Vector2.zero;
+            } else {
+                hubPanels[i].anchoredPosition = hubPanels[i - 1].anchoredPosition + new Vector2(hubPanels[i - 1].rect.width, 0);
+            }
+        }
+    }
+
+    [Button] private void SwipeLeft() {SwipePanel(-1);}
+    [Button] private void SwipeRight() {SwipePanel(1);}
+    
+    private void SwipePanel(int direction)
+    {
+        panelsContainer.DOKill(true);
+        panelsContainer.DOAnchorPosX(panelsContainer.anchoredPosition.x + (panelWidth * direction), swipeTransitionDuration).SetEase(swipeTransitionEaseType);
     }
 }
