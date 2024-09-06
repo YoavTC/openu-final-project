41e75c9861e96ce2e2b9bd6e548a819d9f47504f
Yoav TC <yoavtc2004@gmail.com>
2024-09-06 22:34:57 +0300

    🖼️• Added hub panels swiping

--- Assets/Game Assets/Scripts/HubCameraController.cs
+++ Assets/Game Assets/Scripts/HubCameraController.cs
@@ -3,11 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using NaughtyAttributes;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class HubCameraController : MonoBehaviour
+public class HubCameraController : MonoBehaviour, IEndDragHandler, IDragHandler
 {
     [Header("Settings")]
     [SerializeField] private float swipeTransitionDuration;
@@ -23,6 +24,9 @@ public class HubCameraController : MonoBehaviour
         panelsContainer = transform.GetChild(0).GetComponent<RectTransform>();
         panelWidth = panelsContainer.GetChild(0).GetComponent<RectTransform>().rect.width;
         HubPanelsSetup();
+     
+        panelLocation = panelsContainer.position;
+        panelLocation = new Vector3(0, 0, 0);
     }
 
     private void HubPanelsSetup()
@@ -32,6 +36,10 @@ public class HubCameraController : MonoBehaviour
         for (int i = 0; i < panelsContainer.childCount; i++)
         {
             hubPanels.Add(panelsContainer.GetChild(i).GetComponent<RectTransform>());
+            
+            //Set text TODO: delete later
+            TMP_Text textDisplay = panelsContainer.GetChild(i).GetChild(0).GetComponent<TMP_Text>();
+            textDisplay.text = string.Format(textDisplay.text, i, panelWidth);
             
             if (i == 0)
             {
@@ -49,5 +57,33 @@ public class HubCameraController : MonoBehaviour
     {
         panelsContainer.DOKill(true);
         panelsContainer.DOAnchorPosX(panelsContainer.anchoredPosition.x + (panelWidth * direction), swipeTransitionDuration).SetEase(swipeTransitionEaseType);
+    }
+    
+    
+
+    [SerializeField] private float minDragDifference;
+    [SerializeField] private float swipeEasing;
+    private Vector3 panelLocation;
+    
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        float closestPositionX = Mathf.Round((panelsContainer.anchoredPosition.x * swipeEasing) / panelWidth) * panelWidth;
+        closestPositionX = Mathf.Clamp(closestPositionX,-1 * ((hubPanels.Count - 1) * panelWidth), 0);
+        
+        //TODO: Get the distance of the swipe, if its Abs value is more than half of the panel width, slide to the next
+        //position that can multiply by the width of the panel in the correct direction.
+        
+        panelsContainer.DOAnchorPosX(closestPositionX, swipeTransitionDuration).SetEase(swipeTransitionEaseType);
+        panelLocation = new Vector3(closestPositionX, 0, 0);
+    }
+    
+    public void OnDrag(PointerEventData eventData)
+    {
+        float dragDifference = eventData.pressPosition.x - eventData.position.x;
+        if (Mathf.Abs(dragDifference) > minDragDifference)
+        {
+            panelsContainer.anchoredPosition = panelLocation - new Vector3(dragDifference, 0, 0);
+            Debug.DrawLine(eventData.pressPosition, eventData.position, Color.magenta);
+        }
     }
 }
