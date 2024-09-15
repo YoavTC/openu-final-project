3c7873247bbb7c2538a11914234defb71656f3f2
Yoav TC <yoavtc2004@gmail.com>
2024-09-15 16:07:30 +0300

    🚀• Elixir bar system done

--- Assets/Game Assets/Game Scripts/ElixirManager.cs
+++ Assets/Game Assets/Game Scripts/ElixirManager.cs
@@ -1,18 +1,64 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using External_Packages;
+using NaughtyAttributes;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ElixirManager : MonoBehaviour
+public class ElixirManager : Singleton<ElixirManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    private int _currentElixir;
+    public int currentElixir 
     {
-        
+        set
+        {
+            UpdateElixirBarUI();
+            _currentElixir = value;
+        }
+        get => _currentElixir;
     }
 
-    // Update is called once per frame
-    void Update()
+    [Header("Components")] 
+    [SerializeField] private Slider elixirBarSlider;
+    [SerializeField] private TMP_Text elixirBarAmountDisplay;
+
+    [Header("Settings")]
+    [SerializeField] private int defaultIncreaseAmount;
+    [SerializeField] private float increaseCooldown;
+    [SerializeField] [ReadOnly] private float timeElapsed;
+    
+    private void Update()
     {
-        
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed >= increaseCooldown)
+        {
+            timeElapsed = 0f;
+            IncreaseElixir(defaultIncreaseAmount);
+        }
     }
+
+    private void UpdateElixirBarUI()
+    {
+        elixirBarSlider.value = currentElixir;
+        elixirBarAmountDisplay.text = currentElixir.ToString();
+    }
+
+    [Button]
+    public void DecreaseTest()
+    {
+        DecreaseElixir(10);
+    }
+    
+    [Button]
+    public void IncreaseText()
+    {
+        IncreaseElixir(10);
+    }
+
+    public void IncreaseElixir(int amount) => currentElixir = Mathf.Clamp(currentElixir + amount, 0, 100);
+    public void DecreaseElixir(int amount) => currentElixir = Mathf.Clamp(currentElixir - amount, 0, 100);
+    public bool CanAffordOperation(int amount) => currentElixir - amount > 0;
 }
