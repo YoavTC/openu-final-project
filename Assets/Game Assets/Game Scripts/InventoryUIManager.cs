45f703ac6ff777f414950c68d1b0e0a4cbb05b7f
Yoav TC <yoavtc2004@gmail.com>
2024-09-18 10:05:05 +0300

    🚀• Drag and drop towers from inventory to scene

--- Assets/Game Assets/Game Scripts/InventoryUIManager.cs
+++ Assets/Game Assets/Game Scripts/InventoryUIManager.cs
@@ -1,17 +1,20 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using DG.Tweening;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class InventoryUIManager : MonoBehaviour
+public class InventoryUIManager : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    [Header("Components")]
+    [Header("Drawer Components")]
     [SerializeField] private RectTransform rectTransform;
     [SerializeField] private RectTransform showHideButton;
 
-    [Header("Settings")]
+    [Header("Drawer Settings")]
     [SerializeField] private float buttonCooldown;
     [SerializeField] private Ease easeType;
     [SerializeField] private float inTime;
@@ -27,13 +30,18 @@ public class InventoryUIManager : MonoBehaviour
     {
         isIn = true;
         inY = rectTransform.anchoredPosition.y;
+
+        mainCamera = Camera.main;
     }
 
     private void Update()
     {
         elapsedTime += Time.deltaTime; 
-    } 
-    
+        
+        Debug.DrawLine(beginDragPoint, currentDragPoint, Color.green);
+    }
+
+    #region Inventory Drawer
     public void OnShowHideButtonPress()
     {
         if (elapsedTime >= buttonCooldown)
@@ -51,5 +59,52 @@ public class InventoryUIManager : MonoBehaviour
 
     private void ShowItems() => rectTransform.DOAnchorPosY(inY, inTime).SetEase(easeType);
     private void HideItems() => rectTransform.DOAnchorPosY(outY, outTime).SetEase(easeType);
+    #endregion
+
+    [Header("Card Dragging Components")]
+    [SerializeField] private GameObject draggedCardPrefab;
+    [SerializeField] private GameObject towerPrefab;
+    private GameObject draggedCard;
     
+    private Camera mainCamera;
+    private Vector3 beginDragPoint;
+    private Vector3 currentDragPoint;
+    
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        beginDragPoint = eventData.position;
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+
+        foreach (var VARIABLE in results)
+        {
+            Debug.Log(VARIABLE.gameObject.name);
+        }
+
+        GameObject card = results.First(a => a.gameObject.CompareTag("InventoryUICard")).gameObject;
+        draggedCard = Instantiate(draggedCardPrefab, transform);
+        Debug.Log("Found: " + card);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        currentDragPoint = eventData.position;
+        if (draggedCard != null) draggedCard.transform.position = currentDragPoint;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Vector2 placementPosition = mainCamera.ScreenToWorldPoint(eventData.position);
+        if (IsValidPlacementPosition(placementPosition))
+        {
+            Instantiate(towerPrefab, placementPosition, quaternion.identity);
+        }
+        
+        if (draggedCard != null) Destroy(draggedCard);
+    }
+
+    private bool IsValidPlacementPosition(Vector2 pos)
+    {
+        return true;
+    }
 }
