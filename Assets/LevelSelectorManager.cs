fdd4054848352ef398f66e7413ae8f61ced5953c
Yoav TC <yoavtc2004@gmail.com>
2024-11-04 12:30:45 +0200

    ⚡️• Added Assembly Definition to speed up compile

--- Assets/LevelSelectorManager.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class LevelSelectorManager : MonoBehaviour
-{
-    [SerializeField] private RectTransform[] levelButtons;
-}
