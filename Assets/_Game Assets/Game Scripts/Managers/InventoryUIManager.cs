7168cb23fb9e3ff1f1b48553a4b804af1522e327
Yoav TC <yoavtc2004@gmail.com>
2024-11-11 17:14:07 +0200

    🧹• Removed unnecessary using statements

--- Assets/_Game Assets/Game Scripts/Managers/InventoryUIManager.cs
+++ Assets/_Game Assets/Game Scripts/Managers/InventoryUIManager.cs
@@ -1,9 +1,6 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using DG.Tweening;
-using External_Packages;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.EventSystems;
--- Assets/_Game Assets/Game Scripts/Towers & Enemies/TowerDefault.cs
+++ Assets/_Game Assets/Game Scripts/Towers & Enemies/TowerDefault.cs
@@ -1,5 +1,3 @@
-using UnityEngine;
-
 public class TowerDefault : TowerBase
 {
     private EnemyManager enemyManager;
